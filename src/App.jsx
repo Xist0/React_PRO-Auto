31923f27b310af388c5cc438acd828c54ba0263d
Xist0 <x1spridcherd@gmail.com>
2023-11-28 20:12:31 +0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,31 @@ function App() {
 
   return (
     <>
-    <Header/>
-    <Footer/>
+      <Header />
+      <div className="container-block-rectangle">
+        <div className="block-rectangle-carts">
+          <div className="block-rectangle-cart" id='cart-red'>
+            <img src="/src/img/ico.svg" alt="" />
+            <h1>Наличный и безналичный расчет</h1>
+          </div>
+          <div className="block-rectangle-cart">
+            <img src="/src/img/call-centr.svg" alt="" />
+            <h1>Техническая помощь и консультация</h1>
+          </div>
+          <div className="block-rectangle-cart">
+            <img src="/src/img/надежность.svg" alt="" />
+            <h1>Только качественная и проверенная продукция</h1>
+            <a href="#"><img src="/src/img/arrow.svg" id='carts-arrow' alt="" /></a>
+          </div>
+          <div className="block-rectangle-cart">
+            <img src="/src/img/поддержка.svg" alt="" />
+            <h1>Мы всегда на связи
+              с 9:00 до 18:00!</h1>
+             < a href="#"><img src="/src/img/arrow.svg" id='carts-arrow' alt="" /></a>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
+import './header.css'
 
 export class Header extends Component {
   render() {
     return (
-      <div>Header</div>
+      <header>
+        <div className="header-menu">
+          <div className="header-menu-nav">
+            <img src="/src/img/logo.svg" id ='menu-nav-logo' alt="" />
+            <a href="#"><h1>КОМПАНИЯ</h1></a>
+            <a href="#"><h1>КАТАЛОГ</h1></a>
+            <a href="#"><h1>УСЛУГИ</h1></a>
+            <a href="#"><h1>ИНФОРМАЦИЯ</h1></a>
+            <a href="#"><h1>КОНТАКТЫ</h1></a>
+            <button><a href="#"><h1>ЗАКАЗАТЬ ЗВОНОК</h1></a></button>
+            <img src="/src/img/lup.svg" alt="" />
+            <img src="/src/img/корзина пассивная.svg" alt="" />
+            <div className="menu-nav-quantity"><h1>0</h1></div>
+          </div>
+        </div>
+        <div className="header-main-container">
+          <div className="header-main-content">
+            <h1>НАДЕЖНЫЕ, КАЧЕСТВЕННЫЕ АККУМУЛЯТОРЫ</h1>
+            <div className="header-main-content-button">
+              <button><a href="#"><h1>ПЕРЕЙТИ В КАТАЛОГ</h1></a></button>
+            </div>
+          </div>
+        </div>
+      </header>
     )
   }
 }
